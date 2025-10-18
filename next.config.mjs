10a9be7e9fa20cdccb7d8d745123ada934eab56e
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-10-19 00:44:27 +0900

    feat - google image domain 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,30 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ssl.pstatic.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'phinf.pstatic.net',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
